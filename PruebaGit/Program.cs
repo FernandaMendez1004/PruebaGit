35ca2e4ca9ac5a2050f83ed6653eb5c14fed5c8b
HP <HP@LAPTOP-S59O8GR7>
2026-02-20 11:17:02 -0600

    2commit

--- PruebaGit/Program.cs
+++ PruebaGit/Program.cs
@@ -15,3 +15,7 @@ Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
